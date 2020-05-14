aa46c0c1e8a3b59bbf34dd02e8a929f84795a55f
unknown <ryanhuuu@gmail.com>
2020-05-14 00:55:51 -0400

    swap inventory

--- src/reducers/InventoryReducer.js
+++ src/reducers/InventoryReducer.js
@@ -4,6 +4,10 @@ export default function InventoryReducer(
     backpack: 
     [ items.Apple,items.Potion,items.Bread,
       items.Rock,null,null,
+      null,null,null,
+
+      items.Apple,items.Potion,items.Bread,
+      items.Rock,null,null,
       null,null,null ]
   },
   action
@@ -12,7 +16,7 @@ export default function InventoryReducer(
   switch (action.type) {
     case 'SET_ACTIVE_ITEM':
       return {
-        activeIndex: action.itemIndex,
+        activeIndex: action.inventoryIndex,
         backpack: [...state.backpack]
       }
     case 'DELETE_ITEM':
@@ -25,6 +29,15 @@ export default function InventoryReducer(
     case 'GET_ITEM':
       tempInventory = [...state.backpack]
       tempInventory[tempInventory.indexOf(null)] = items[action.itemIndex]
+      return {
+        ...state,
+        backpack: tempInventory
+      }
+    case 'SWAP_ITEMS':
+      tempInventory = [...state.backpack]
+      let tempValue = tempInventory[action.inventoryIndex1]
+      tempInventory[action.inventoryIndex1] = tempInventory[action.inventoryIndex2]
+      tempInventory[action.inventoryIndex2] = tempValue
       return {
         ...state,
         backpack: tempInventory
@@ -43,6 +56,7 @@ let items = {
     effectDescription: 'heals 5 HP, 5 Hunger',
     HPUp: 5,
     HungerUp: 5,
+    gold: 1,
   },
   'Potion': {
     name: 'Potion',
@@ -51,6 +65,7 @@ let items = {
     flavor: 'a potion made of red herbs',
     effectDescription: 'heals 50 HP',
     HPUp: 50,
+    gold: 10,
   },
   'Bread': {
     name: 'Bread',
@@ -60,12 +75,13 @@ let items = {
     effectDescription: 'heals 10 HP, 20 Hunger',
     HPUp: 10,
     HungerUp: 20,
+    gold: 3,
   },
   'Rock': {
     name: 'Rock',
     type: 'crafting',
     image: 'rock',
     flavor: 'kinda hard.',
-    effectDescription: 'heals 10 HP, 20 Hunger',
+    gold: 1,
   },
 }
