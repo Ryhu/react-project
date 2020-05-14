8687667cbb4eb65aa496079e6c2e3037e0e80ede
unknown <ryanhuuu@gmail.com>
2020-05-14 00:53:54 -0400

    cleaned up z-index troubles on inventory

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,22 +7,42 @@ class Inventory extends Component {
   render() {
     return (
       <InventoryDisplay>
-        <BackgroundImage src="./backpack.png"/>
-        <Row>
-          <InventorySlot item={this.props.inventory.backpack[1-1]} inventoryIndex='1' />
-          <InventorySlot item={this.props.inventory.backpack[2-1]} inventoryIndex='2' />
-          <InventorySlot item={this.props.inventory.backpack[3-1]} inventoryIndex='3' />
-        </Row>
-        <Row>
-          <InventorySlot item={this.props.inventory.backpack[4-1]} inventoryIndex='4' />
-          <InventorySlot item={this.props.inventory.backpack[5-1]} inventoryIndex='5' />
-          <InventorySlot item={this.props.inventory.backpack[6-1]} inventoryIndex='6' />
-        </Row>
-        <Row>
-          <InventorySlot item={this.props.inventory.backpack[7-1]} inventoryIndex='7' />
-          <InventorySlot item={this.props.inventory.backpack[8-1]} inventoryIndex='8' />
-          <InventorySlot item={this.props.inventory.backpack[9-1]} inventoryIndex='9' />
-        </Row>
+        <BackpackDisplay>
+          <BackgroundImage src="./backpack.png"/>
+          <Row>
+            <InventorySlot item={this.props.inventory.backpack[1-1]} inventoryIndex='1' />
+            <InventorySlot item={this.props.inventory.backpack[2-1]} inventoryIndex='2' />
+            <InventorySlot item={this.props.inventory.backpack[3-1]} inventoryIndex='3' />
+          </Row>
+          <Row>
+            <InventorySlot item={this.props.inventory.backpack[4-1]} inventoryIndex='4' />
+            <InventorySlot item={this.props.inventory.backpack[5-1]} inventoryIndex='5' />
+            <InventorySlot item={this.props.inventory.backpack[6-1]} inventoryIndex='6' />
+          </Row>
+          <Row>
+            <InventorySlot item={this.props.inventory.backpack[7-1]} inventoryIndex='7' />
+            <InventorySlot item={this.props.inventory.backpack[8-1]} inventoryIndex='8' />
+            <InventorySlot item={this.props.inventory.backpack[9-1]} inventoryIndex='9' />
+          </Row>
+        </BackpackDisplay>
+        <BackpackDisplay>
+          <BackgroundImage src="./backpack.png"/>
+          <Row>
+            <InventorySlot item={this.props.inventory.backpack[10-1]} inventoryIndex='10' />
+            <InventorySlot item={this.props.inventory.backpack[11-1]} inventoryIndex='11' />
+            <InventorySlot item={this.props.inventory.backpack[12-1]} inventoryIndex='12' />
+          </Row>
+          <Row>
+            <InventorySlot item={this.props.inventory.backpack[13-1]} inventoryIndex='13' />
+            <InventorySlot item={this.props.inventory.backpack[14-1]} inventoryIndex='14' />
+            <InventorySlot item={this.props.inventory.backpack[15-1]} inventoryIndex='15' />
+          </Row>
+          <Row>
+            <InventorySlot item={this.props.inventory.backpack[16-1]} inventoryIndex='16' />
+            <InventorySlot item={this.props.inventory.backpack[17-1]} inventoryIndex='17' />
+            <InventorySlot item={this.props.inventory.backpack[18-1]} inventoryIndex='18' />
+          </Row>
+        </BackpackDisplay>
       </InventoryDisplay>
     )
   }
@@ -33,24 +53,24 @@ const BackgroundImage = styled.img`
   opacity: 0.5;
   height: 16rem;
   max-width: 16rem;
-  z-index: 1;
+  z-index: -1
 `
 const Row = styled.div`
   display: flex;
   div{
     margin: 5px;
   }
-  z-index: 2;
 `
-const InventoryDisplay = styled.div`
-  background-color: green;
-  padding: 1rem;
+const BackpackDisplay = styled.div`
   max-width: 16rem;
   display: flex;
   flex-direction: column;
   div{
     margin: 5px;
   }
+`
+const InventoryDisplay = styled.div`
+  float: right;
 `
 
 const mapStateToProps = (state) => ({inventory: state.inventory});
