d0dba7fccbe4c27ba4b59a86369b81df8a30ec9d
unknown <ryanhuuu@gmail.com>
2020-05-19 21:05:25 -0400

    Updates

---
+++ precache-manifest.7473ff0c20a9cf4ee81812da2cc1d090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78695a79e0e486e3b7c8d2497b2b561f",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "3603d4d21927b90f0656",
+    "url": "/react-project/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3358e1325320aa72231e",
+    "url": "/react-project/static/js/2.15a53695.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project/static/js/2.15a53695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3603d4d21927b90f0656",
+    "url": "/react-project/static/js/main.eeeb69c5.chunk.js"
+  },
+  {
+    "revision": "da856c4191aed5542a94",
+    "url": "/react-project/static/js/runtime-main.36005df5.js"
+  }
+]);
